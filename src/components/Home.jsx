a9b00847163ac0edfaafd243b64f16fb51cc7568
Yash Kaskar <yash.kaskar99@gmail.com>
2022-11-25 10:47:47 +0530

    age

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,18 @@ import { useState } from "react"
 const Home = () => {
 
   const [name, setName] = useState('Naruto');
+  const [age, setAge] = useState(15);
+
+
+  const click = () => { 
+    setName('Obito');
+    setAge(25)
+  }
 
   return (
     <div> 
-          <h1> {name}</h1>
-          <button onClick={() => setName('Obito')}> Click to change name</button>
+      <h1> {name} and he's from Annime called Naruto and his age is {age} years old </h1>
+          <button onClick={click}> Click to change name and age</button>
       
     </div>
   )
